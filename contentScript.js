96d2aa4345b65a140360bd32617b40ffce65d0bd
Seika <artistnoob6@gmail.com>
2026-01-19 23:21:40 +0530

    Revert "Feature : Able to scroll in normal sites"
    This reverts commit db59aaada7725fba20286c63a127459e47738197.

--- contentScript.js
+++ contentScript.js
@@ -1,75 +1,44 @@
 document.addEventListener("keydown",(e)=>{
 
 if(e.ctrlKey && e.altKey && e.key=="q"){
-    const selection = window.getSelection();
-    const text = selection.toString();
+    const selection = window.getSelection();
+    const text = selection.toString();
 
-    if (text.length > 0) {
-      const range = selection.getRangeAt(0);
-      const rect = range.getBoundingClientRect();
+    if (text.length > 0) {
+      const range = selection.getRangeAt(0);
+      const rect = range.getBoundingClientRect();
 
-      const absoluteTop = rect.top + window.scrollY;
+      const absoluteTop = rect.top + window.scrollY;
+      const absoluteLeft = rect.left + window.scrollX;
 
-      console.log("Selected Text:", text);
-      console.log("Scroll To Y:", absoluteTop);
+      console.log("Selected Text:", text);
+      console.log("Scroll To Y:", absoluteTop);
+      console.log("Scroll To X:", absoluteLeft);
 
-	chrome.storage.local.set({
-		"saved_pin":{
-		text_:text,
-		scrolly_:absoluteTop-250,
-		}
-
-	},()=>{console.log("saved")});
-      toast(`Saved Pin : ${text.substring(0,15)}...`);
-
-    }
+      toast(`Saved position: ${Math.round(absoluteTop)}px`);}
 else{
 console.log("no content selected")
-}
-}
-}
-)
-document.addEventListener("keydown",(e)=>{
-if(e.ctrlKey && e.altKey && e.key=="a"){
-
-chrome.storage.local.get(["saved_pin"],(res)=>{
-
-if(res.saved_pin){
-	window.scrollTo({
-		top:res.saved_pin.scrolly_,
-		behavior:'smooth'
-
-	});
-	toast("Scrolled to Pin");
-}
-
+}}
 
 
 })
-}
-
-})
-
-
-
-
-
 function toast(msg) {
-  const el = document.createElement("div");
-  el.textContent = msg;
+  const el = document.createElement("div");
+  el.textContent = msg;
 
-  el.style.all = "initial";
-  el.style.position = "fixed";
-  el.style.top = "20px";
-  el.style.left = "50%";
-  el.style.transform="translateX(-50%)";
-  el.style.background = "#111";
-  el.style.color = "#fff";
-  el.style.padding = "15px 20px";
-  el.style.borderRadius = "6px";
-  el.style.zIndex = "2147483647";
-  el.style.fontFamily = "sans-serif";
+  el.style.all = "initial";
+  el.style.position = "fixed";
+  el.style.top = "20px";
+  el.style.left = "50%";
+  el.style.transform="translateX(50%)";
+  el.style.background = "#111";
+  el.style.color = "#fff";
+  el.style.padding = "15px 20px";
+  el.style.borderRadius = "6px";
+  el.style.zIndex = "2147483647";
+  el.style.fontFamily = "sans-serif";
 
-  document.documentElement.appendChild(el);
-  setTimeout(() => el.remove(), 1500);
+  document.documentElement.appendChild(el);
+  setTimeout(() => el.remove(), 1500);
 }
+
