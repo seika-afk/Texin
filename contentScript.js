de8db537f4fd36e1afeebeecbd6cd1399a11d548
Seika <artistnoob6@gmail.com>
2026-01-21 03:03:02 +0530

    feature : Click and retrieve

--- contentScript.js
+++ contentScript.js
@@ -1,44 +1,73 @@
+let x=0 
+function scroll(x){
+	const container=document.querySelector('[class*="scrollbar-gutter"]');
+
+	if(container){
+		container.scrollTop=x;
+			console.log("Scrolled to : ",x);
+
+	}
+
+
+
+}
+
+
 document.addEventListener("keydown",(e)=>{
+	if (e.ctrlKey && e.altKey && e.key=="q"){
+		const selection=window.getSelection();
+		const text= selection.toString();
+		if(text.length>0){
 
-if(e.ctrlKey && e.altKey && e.key=="q"){
-    const selection = window.getSelection();
-    const text = selection.toString();
 
-    if (text.length > 0) {
-      const range = selection.getRangeAt(0);
-      const rect = range.getBoundingClientRect();
+			const container=document.querySelector('[class*="scrollbar-gutter"]');
 
-      const absoluteTop = rect.top + window.scrollY;
-      const absoluteLeft = rect.left + window.scrollX;
 
-      console.log("Selected Text:", text);
-      console.log("Scroll To Y:", absoluteTop);
-      console.log("Scroll To X:", absoluteLeft);
+			if(container){
+				x=container.scrollTop;
+				console.log("saved text ::: ",text)
+				toast(`Saved position : ${Math.round(x)}px`);
 
-      toast(`Saved position: ${Math.round(absoluteTop)}px`);}
-else{
-console.log("no content selected")
-}}
+			}
+
+
+		}
+
+
+
+	}
+	    if (e.ctrlKey && e.altKey && e.key == "a") {
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        scroll(x);
+        toast(`Jumped to: ${Math.round(x)}px`);
+    }
+
+
 
 
 })
-function toast(msg) {
-  const el = document.createElement("div");
-  el.textContent = msg;
 
-  el.style.all = "initial";
-  el.style.position = "fixed";
-  el.style.top = "20px";
-  el.style.left = "50%";
-  el.style.transform="translateX(50%)";
-  el.style.background = "#111";
-  el.style.color = "#fff";
-  el.style.padding = "15px 20px";
-  el.style.borderRadius = "6px";
-  el.style.zIndex = "2147483647";
-  el.style.fontFamily = "sans-serif";
 
-  document.documentElement.appendChild(el);
-  setTimeout(() => el.remove(), 1500);
+function toast(msg){
+const el= document.createElement("div");
+   el.textContent = msg;
+    el.style.all = "initial";
+    el.style.position = "fixed";
+    el.style.top = "20px";
+    el.style.left = "50%";
+    el.style.transform = "translateX(-50%)";
+    el.style.background = "#111";
+    el.style.color = "#fff";
+    el.style.padding = "15px 20px";
+    el.style.borderRadius = "6px";
+    el.style.zIndex = "2147483647";
+    el.style.fontFamily = "sans-serif";
+    document.documentElement.appendChild(el);
+    setTimeout(() => el.remove(), 1500);
+
+
+
+
 }
-
